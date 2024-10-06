a336edea9a89fde7a84781cea04f424239e01e07
SebastiaoTrindade <sebatrindade@hotmail.com>
2024-10-05 22:56:12 -0300

    Criação do Projeto

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # Sessão login
+  root 'sessions#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+end
